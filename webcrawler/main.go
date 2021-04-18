dd35516224f6b47126b73f8c17c76ea79d2af6d8
Matheus Nogueira <matheus.nogueira@wildlifestudios.com>
2021-04-18 01:09:27 -0300

    Setup configuration from env variables

--- webcrawler/main.go
+++ webcrawler/main.go
@@ -1,5 +1,33 @@
 package main
 
-func main() {
+import (
+	"strings"
 
+	"github.com/mathnogueira/imdb-api/webcrawler/crawler"
+	"github.com/spf13/viper"
+)
+
+func main() {
+	config, err := getConfig()
+
+	if err != nil {
+		panic(err)
+	}
+
+	crawlerOptions := crawler.Options{StorageURL: config.GetString("storage.url")}
+	err = crawler.Execute(crawlerOptions)
+
+	if err != nil {
+		panic(err)
+	}
+}
+
+func getConfig() (*viper.Viper, error) {
+	config := viper.New()
+
+	config.SetEnvPrefix("crawler")
+	config.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
+	config.AutomaticEnv()
+
+	return config, nil
 }
