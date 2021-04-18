58d3cdb74c88259a17b88425ebf172574f636710
Matheus Nogueira <matheus.nogueira@wildlifestudios.com>
2021-04-18 01:06:55 -0300

    Add crawler logic

---
+++ webcrawler/crawler/crawler_test.go
@@ -0,0 +1,53 @@
+package crawler_test
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"sync"
+
+	"github.com/mathnogueira/imdb-api/webcrawler/crawler"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Crawler", func() {
+
+	It("Should execute without any error when storage API is online", func() {
+		server := setupMockServer()
+		defer server.Close()
+
+		crawlerOptions := crawler.Options{StorageURL: server.URL}
+		err := crawler.Execute(crawlerOptions)
+
+		Expect(err).ShouldNot(HaveOccurred())
+	})
+
+	It("Should return an error if storage API is offline", func() {
+		server := setupMockServer()
+
+		crawlerOptions := crawler.Options{StorageURL: server.URL}
+		// Forces the server to close before the execution of the crawler
+		// This simulates downtime in the storage API
+		server.Close()
+
+		err := crawler.Execute(crawlerOptions)
+
+		Expect(err).Should(HaveOccurred())
+	})
+})
+
+func setupMockServer() *httptest.Server {
+	var server *httptest.Server
+	var wg sync.WaitGroup
+	wg.Add(1)
+
+	go func() {
+		server = httptest.NewServer(http.HandlerFunc(func(writer http.ResponseWriter, req *http.Request) {
+			writer.WriteHeader(201)
+		}))
+		wg.Done()
+	}()
+
+	wg.Wait()
+	return server
+}
