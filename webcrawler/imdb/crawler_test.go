5ee3a1ac4e324dcf18a233dbbf9e156e44c7f73e
Matheus Nogueira <matheus.nogueira@wildlifestudios.com>
2021-04-21 17:29:27 -0300

    Rename crawler to extract in tests and inject logger in test code

--- webcrawler/imdb/crawler_test.go
+++ webcrawler/imdb/crawler_test.go
@@ -4,12 +4,15 @@ import (
 	"github.com/mathnogueira/imdb-api/webcrawler/imdb"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+	"go.uber.org/zap"
 )
 
 var _ = Describe("IMDB Crawler", func() {
 
+	logger := zap.NewNop()
+
 	It("Should be able to parse movies from IMDB page", func() {
-		crawler := imdb.NewCrawler()
+		crawler := imdb.NewCrawler(logger)
 		extractedMovies := crawler.GetTopMovies()
 
 		Expect(len(extractedMovies)).To(Equal(1000))
